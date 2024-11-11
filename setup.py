83da9e321401b3ed5c487809a36c925f1938eafb
rossidae <rossidae@protonmail.com>
2024-11-11 17:53:09 +0000

    Adjusted packaging for import in python3.11+

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='geochemistry_helpers',
     version='0.1.0',
-    packages=find_packages(include=['geochemistry_helpers/*']),
+    packages=find_packages(where='.', include=["geochemistry_helpers", "geochemistry_helpers.*"]),
     install_requires=[
         'PyYAML',
         'numpy>=1.14.5',
